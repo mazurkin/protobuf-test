4886633ea1058ef12a06164095ae67b51137189e
Nickolay Mazurkin <mazurkin@gmail.com>
2016-09-13 22:12:36 +0300

    minor

---
+++ src/main/java/org/test/beans/MonsterEntity.java
@@ -0,0 +1,607 @@
+package org.test.beans;
+
+import java.io.Serializable;
+
+public class MonsterEntity implements Serializable {
+
+    public String value001;
+
+    public int    value002;
+
+    public long   value003;
+
+    public double value004;
+
+    public String value005;
+
+    public int    value006;
+
+    public long   value007;
+
+    public double value008;
+
+    public String value009;
+
+    public int    value010;
+
+    public long   value011;
+
+    public double value012;
+
+    public String value013;
+
+    public int    value014;
+
+    public long   value015;
+
+    public double value016;
+
+    public String value017;
+
+    public int    value018;
+
+    public long   value019;
+
+    public double value020;
+
+    public String value021;
+
+    public int    value022;
+
+    public long   value023;
+
+    public double value024;
+
+    public String value025;
+
+    public int    value026;
+
+    public long   value027;
+
+    public double value028;
+
+    public String value029;
+
+    public int    value030;
+
+    public long   value031;
+
+    public double value032;
+
+    public String value033;
+
+    public int    value034;
+
+    public long   value035;
+
+    public double value036;
+
+    public String value037;
+
+    public int    value038;
+
+    public long   value039;
+
+    public double value040;
+
+    public String value041;
+
+    public int    value042;
+
+    public long   value043;
+
+    public double value044;
+
+    public String value045;
+
+    public int    value046;
+
+    public long   value047;
+
+    public double value048;
+
+    public String value049;
+
+    public int    value050;
+
+    public long   value051;
+
+    public double value052;
+
+    public String value053;
+
+    public int    value054;
+
+    public long   value055;
+
+    public double value056;
+
+    public String value057;
+
+    public int    value058;
+
+    public long   value059;
+
+    public double value060;
+
+    public String value061;
+
+    public int    value062;
+
+    public long   value063;
+
+    public double value064;
+
+    public String value065;
+
+    public int    value066;
+
+    public long   value067;
+
+    public double value068;
+
+    public String value069;
+
+    public int    value070;
+
+    public long   value071;
+
+    public double value072;
+
+    public String value073;
+
+    public int    value074;
+
+    public long   value075;
+
+    public double value076;
+
+    public String value077;
+
+    public int    value078;
+
+    public long   value079;
+
+    public double value080;
+
+    public String value081;
+
+    public int    value082;
+
+    public long   value083;
+
+    public double value084;
+
+    public String value085;
+
+    public int    value086;
+
+    public long   value087;
+
+    public double value088;
+
+    public String value089;
+
+    public int    value090;
+
+    public long   value091;
+
+    public double value092;
+
+    public String value093;
+
+    public int    value094;
+
+    public long   value095;
+
+    public double value096;
+
+    public String value097;
+
+    public int    value098;
+
+    public long   value099;
+
+    public double value100;
+
+    public String value101;
+
+    public int    value102;
+
+    public long   value103;
+
+    public double value104;
+
+    public String value105;
+
+    public int    value106;
+
+    public long   value107;
+
+    public double value108;
+
+    public String value109;
+
+    public int    value110;
+
+    public long   value111;
+
+    public double value112;
+
+    public String value113;
+
+    public int    value114;
+
+    public long   value115;
+
+    public double value116;
+
+    public String value117;
+
+    public int    value118;
+
+    public long   value119;
+
+    public double value120;
+
+    public String value121;
+
+    public int    value122;
+
+    public long   value123;
+
+    public double value124;
+
+    public String value125;
+
+    public int    value126;
+
+    public long   value127;
+
+    public double value128;
+
+    public String value129;
+
+    public int    value130;
+
+    public long   value131;
+
+    public double value132;
+
+    public String value133;
+
+    public int    value134;
+
+    public long   value135;
+
+    public double value136;
+
+    public String value137;
+
+    public int    value138;
+
+    public long   value139;
+
+    public double value140;
+
+    public String value141;
+
+    public int    value142;
+
+    public long   value143;
+
+    public double value144;
+
+    public String value145;
+
+    public int    value146;
+
+    public long   value147;
+
+    public double value148;
+
+    public String value149;
+
+    public int    value150;
+
+    public long   value151;
+
+    public double value152;
+
+    public String value153;
+
+    public int    value154;
+
+    public long   value155;
+
+    public double value156;
+
+    public String value157;
+
+    public int    value158;
+
+    public long   value159;
+
+    public double value160;
+
+    public String value161;
+
+    public int    value162;
+
+    public long   value163;
+
+    public double value164;
+
+    public String value165;
+
+    public int    value166;
+
+    public long   value167;
+
+    public double value168;
+
+    public String value169;
+
+    public int    value170;
+
+    public long   value171;
+
+    public double value172;
+
+    public String value173;
+
+    public int    value174;
+
+    public long   value175;
+
+    public double value176;
+
+    public String value177;
+
+    public int    value178;
+
+    public long   value179;
+
+    public double value180;
+
+    public String value181;
+
+    public int    value182;
+
+    public long   value183;
+
+    public double value184;
+
+    public String value185;
+
+    public int    value186;
+
+    public long   value187;
+
+    public double value188;
+
+    public String value189;
+
+    public int    value190;
+
+    public long   value191;
+
+    public double value192;
+
+    public String value193;
+
+    public int    value194;
+
+    public long   value195;
+
+    public double value196;
+
+    public String value197;
+
+    public int    value198;
+
+    public long   value199;
+
+    public double value200;
+
+    public String value201;
+
+    public int    value202;
+
+    public long   value203;
+
+    public double value204;
+
+    public String value205;
+
+    public int    value206;
+
+    public long   value207;
+
+    public double value208;
+
+    public String value209;
+
+    public int    value210;
+
+    public long   value211;
+
+    public double value212;
+
+    public String value213;
+
+    public int    value214;
+
+    public long   value215;
+
+    public double value216;
+
+    public String value217;
+
+    public int    value218;
+
+    public long   value219;
+
+    public double value220;
+
+    public String value221;
+
+    public int    value222;
+
+    public long   value223;
+
+    public double value224;
+
+    public String value225;
+
+    public int    value226;
+
+    public long   value227;
+
+    public double value228;
+
+    public String value229;
+
+    public int    value230;
+
+    public long   value231;
+
+    public double value232;
+
+    public String value233;
+
+    public int    value234;
+
+    public long   value235;
+
+    public double value236;
+
+    public String value237;
+
+    public int    value238;
+
+    public long   value239;
+
+    public double value240;
+
+    public String value241;
+
+    public int    value242;
+
+    public long   value243;
+
+    public double value244;
+
+    public String value245;
+
+    public int    value246;
+
+    public long   value247;
+
+    public double value248;
+
+    public String value249;
+
+    public int    value250;
+
+    public long   value251;
+
+    public double value252;
+
+    public String value253;
+
+    public int    value254;
+
+    public long   value255;
+
+    public double value256;
+
+    public String value257;
+
+    public int    value258;
+
+    public long   value259;
+
+    public double value260;
+
+    public String value261;
+
+    public int    value262;
+
+    public long   value263;
+
+    public double value264;
+
+    public String value265;
+
+    public int    value266;
+
+    public long   value267;
+
+    public double value268;
+
+    public String value269;
+
+    public int    value270;
+
+    public long   value271;
+
+    public double value272;
+
+    public String value273;
+
+    public int    value274;
+
+    public long   value275;
+
+    public double value276;
+
+    public String value277;
+
+    public int    value278;
+
+    public long   value279;
+
+    public double value280;
+
+    public String value281;
+
+    public int    value282;
+
+    public long   value283;
+
+    public double value284;
+
+    public String value285;
+
+    public int    value286;
+
+    public long   value287;
+
+    public double value288;
+
+    public String value289;
+
+    public int    value290;
+
+    public long   value291;
+
+    public double value292;
+
+    public String value293;
+
+    public int    value294;
+
+    public long   value295;
+
+    public double value296;
+
+    public String value297;
+
+    public int    value298;
+
+    public long   value299;
+
+    public double value300;
+
+}
--- src/test/java/org/test/CycleTest.java
+++ src/test/java/org/test/CycleTest.java
@@ -12,27 +12,55 @@ public class CycleTest {
     @Test
     public void test() throws Exception {
         CycleProto.InnerBean.Builder innerBean = CycleProto.InnerBean.newBuilder()
-                .setValue1(100000000000000L)
-                .setValue2(200000000000000L)
-                .setValue3(300000000000000L);
+                .setValue1(1L)
+                .setValue2(Long.MAX_VALUE)
+                .setValue3(300000000000000L)
+                .setValue5("0123456789abcdef");
 
         CycleProto.OuterBean outerBean = CycleProto.OuterBean.newBuilder()
-                .setValue1(innerBean) // the same bean
-                .setValue2(innerBean) // the same bean
-                .setValue3(innerBean) // the same bean
+                .setBean1(innerBean) // the same bean
+                .setBean2(innerBean) // the same bean
+                .addBeans(innerBean) // the same bean
+                .addBeans(innerBean) // the same bean
+                .build();
+
+        ByteOutputStream os = new ByteOutputStream();
+        outerBean.writeTo(os);
+
+        System.out.printf("size = %d\n", os.size());
+
+        ByteInputStream is = new ByteInputStream(os.getBytes(), os.size());
+        CycleProto.OuterBean outerBean2 = CycleProto.OuterBean.parseFrom(is);
+
+        // 1. references are lost (may be important for a complex structure)
+        // 2. the final size is x3 times more than actually required
+        Assert.assertSame(outerBean2.getBean1(), outerBean2.getBean2());
+    }
+
+    @Test
+    public void testLoop() throws Exception {
+        CycleProto.InnerBean.Builder innerBean1 = CycleProto.InnerBean.newBuilder()
+                .setValue1(100);
+
+        CycleProto.InnerBean.Builder innerBean2 = CycleProto.InnerBean.newBuilder()
+                .setValue1(200);
+
+        innerBean1.setReference(innerBean2);
+        innerBean2.setReference(innerBean1);
+
+        CycleProto.OuterBean outerBean = CycleProto.OuterBean.newBuilder()
+                .setBean1(innerBean1)
+                .setBean2(innerBean2)
                 .build();
 
         ByteOutputStream os = new ByteOutputStream();
         outerBean.writeTo(os);
 
         ByteInputStream is = new ByteInputStream(os.getBytes(), os.size());
-
         CycleProto.OuterBean outerBean2 = CycleProto.OuterBean.parseFrom(is);
 
-        // 1. references are lost (may be important for a complex structure)
-        // 2. the final size is x3 times more than actually required
-        Assert.assertSame(outerBean2.getValue1(), outerBean2.getValue2());
-        Assert.assertSame(outerBean2.getValue1(), outerBean2.getValue2());
+        // Check for cyclic links
+        Assert.assertSame(outerBean2.getBean1(), outerBean2.getBean2().getReference());
+        Assert.assertSame(outerBean2.getBean2(), outerBean2.getBean1().getReference());
     }
-
 }
